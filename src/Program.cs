a35350b1501ee58aae895756044dca3cbac0804f
lukewire129 <lukewire129@gmail.com>
2025-10-02 14:55:09 +0900

    update .

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,14 @@
-﻿return await Bootstrapper
-  .Factory
-  .CreateWeb (args)
-  .RunAsync ();
+﻿using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+using Statiq.Markdown;
+
+return await Bootstrapper
+    .Factory
+    .CreateWeb (args)
+    .ConfigureSettings (settings =>
+    {
+        settings[Keys.LinkRoot] = "/conf";
+        settings[Keys.LinksUseHttps] = false; // 필요시
+    })
+    .RunAsync ();
